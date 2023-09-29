4feb4bca5f3da6570ff26f2fb2ba80db3591c549
Jenna <jennagodfrey324@gmail.com>
2023-09-28 22:47:28 -0600

    removed signup route

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -54,25 +54,6 @@ def logout():
     return {'message': 'User logged out'}
 
 
-@auth_routes.route('/signup', methods=['POST'])
-def sign_up():
-    """
-    Creates a new user and logs them in
-    """
-    form = SignUpForm()
-    form['csrf_token'].data = request.cookies['csrf_token']
-    if form.validate_on_submit():
-        user = User(
-            username=form.data['username'],
-            email=form.data['email'],
-            password=form.data['password']
-        )
-        db.session.add(user)
-        db.session.commit()
-        login_user(user)
-        return user.to_dict()
-    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
-
 
 @auth_routes.route('/unauthorized')
 def unauthorized():
