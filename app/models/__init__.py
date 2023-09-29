5221d549f1d29d8bc03aa9ede561e26bed24b095
Jenna <jennagodfrey324@gmail.com>
2023-09-29 11:38:57 -0600

    added join table, need to work on foreign keys now

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -7,4 +7,6 @@ from .state import State
 from .contact import Contact
 from .description import Description
 from .list import List
+from .category import Category
+from .entry import Entry
 from .db import environment, SCHEMA
