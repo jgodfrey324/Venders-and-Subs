950702459a88ce3f4c9f3cb35e583a0f740f2a32
Jenna <jennagodfrey324@gmail.com>
2023-09-29 11:40:05 -0600

    added nullable false

--- app/models/category.py
+++ app/models/category.py
@@ -8,7 +8,7 @@ class Category(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    category = db.Column(db.String(50))
+    category = db.Column(db.String(50), nullable=False)
 
     def to_dict(self):
         return {
