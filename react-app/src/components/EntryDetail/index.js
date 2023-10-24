7744b6d9c2b007d4917fc281824844d51bbdf8e9
Jenna <jennagodfrey324@gmail.com>
2023-10-24 16:36:14 -0600

    trying to get details page to coopporate

--- react-app/src/components/EntryDetail/index.js
+++ react-app/src/components/EntryDetail/index.js
@@ -1,17 +1,29 @@
-import { useSelector } from 'react-redux';
-import { Redirect, useParams } from 'react-router-dom';
-// import "./SearchPage.css"
+import { useSelector, useDispatch } from 'react-redux';
+import { Redirect, useParams, useHistory } from 'react-router-dom';
+import "./EntryDetail.css"
+import { useEffect } from 'react';
+import { getOneEntry } from '../../store/entries';
 
 export default function EntryDetail () {
     const entryId = useParams()
+    const history = useHistory()
+    const dispatch = useDispatch()
 	const sessionUser = useSelector(state => state.session.user);
-    const allEntries = useSelector(state => state.entries);
+    const entryClicked = useSelector(state => state.entires)
 
-    console.log(entryId)
+    useEffect(() => {
+        dispatch(getOneEntry(entryId.entryId))
+    }, [entryId])
 
     if (!sessionUser) return <Redirect to='/login' />
 
     return (
-        <>Entry Detail Page...</>
+        <div className='details-page-house'>
+            <div className='entry-detail-house'>
+                <h1>{entryClicked.company.company_name}</h1>
+            </div>
+
+            <p className='navigation-link' onClick={() => history.goBack()}>Go Back</p>
+        </div>
     )
 }
