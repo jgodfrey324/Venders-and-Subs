30d3ecf377d7124656b2fc5e88a6db77cf138b33
Jenna <jennagodfrey324@gmail.com>
2023-10-24 13:45:03 -0600

    first get all entry route

--- app/api/entry_routes.py
+++ app/api/entry_routes.py
@@ -7,19 +7,9 @@ entry_routes = Blueprint('entries', __name__)
 
 @entry_routes.route('/')
 @login_required
-def users():
+def entries():
     """
-    Query for all users and returns them in a list of user dictionaries
+    Query for all entries and returns them in a list of entry dictionaries
     """
-    users = User.query.all()
-    return {'users': [user.to_dict() for user in users]}
-
-
-@entry_routes.route('/<int:id>')
-@login_required
-def user(id):
-    """
-    Query for a user by id and returns that user in a dictionary
-    """
-    user = User.query.get(id)
-    return user.to_dict()
+    entries = Entry.query.all()
+    return {'entries': [entry.to_dict() for entry in entries]}
