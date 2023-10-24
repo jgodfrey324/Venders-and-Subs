27870d0e749fc0bd0c0956bd8b52b64eaa3fec52
Jenna <jennagodfrey324@gmail.com>
2023-10-24 13:43:46 -0600

    added url blueprint

---
+++ app/api/entry_routes.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import Entry
+
+entry_routes = Blueprint('entries', __name__)
+
+
+@entry_routes.route('/')
+@login_required
+def users():
+    """
+    Query for all users and returns them in a list of user dictionaries
+    """
+    users = User.query.all()
+    return {'users': [user.to_dict() for user in users]}
+
+
+@entry_routes.route('/<int:id>')
+@login_required
+def user(id):
+    """
+    Query for a user by id and returns that user in a dictionary
+    """
+    user = User.query.get(id)
+    return user.to_dict()
