bd75a90398c4511509f2ac1d194de6ea67c8c00c
Jenna <jennagodfrey324@gmail.com>
2023-10-25 14:35:10 -0600

    details of single entry page working

--- react-app/src/components/EntryDetail/index.js
+++ react-app/src/components/EntryDetail/index.js
@@ -2,28 +2,31 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Redirect, useParams, useHistory } from 'react-router-dom';
 import "./EntryDetail.css"
 import { useEffect } from 'react';
-import { getOneEntry } from '../../store/entries';
+import { getAllEntries } from '../../store/entries';
 
 export default function EntryDetail () {
-    const entryId = useParams()
+    const urlParam = useParams()
     const history = useHistory()
     const dispatch = useDispatch()
 	const sessionUser = useSelector(state => state.session.user);
-    const entryClicked = useSelector(state => state.entires)
+    const entries = useSelector(state => state.entries)
+
+    const entryId = parseInt(urlParam['entryId'])
+    const selectedEntry = entries ? entries[entryId] : null
 
     useEffect(() => {
-        dispatch(getOneEntry(entryId.entryId))
-    }, [entryId])
+        dispatch(getAllEntries())
+    }, [])
 
     if (!sessionUser) return <Redirect to='/login' />
 
-    return (
+    return selectedEntry ? (
         <div className='details-page-house'>
             <div className='entry-detail-house'>
-                <h1>{entryClicked.company.company_name}</h1>
+                <h1>{selectedEntry.company.company_name}</h1>
             </div>
 
             <p className='navigation-link' onClick={() => history.goBack()}>Go Back</p>
         </div>
-    )
+    ) : null
 }
