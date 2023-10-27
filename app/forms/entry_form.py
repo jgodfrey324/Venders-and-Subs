e43682074a8944fe60d7290cfb9ca155043d0f72
Jenna <jennagodfrey324@gmail.com>
2023-10-27 13:33:21 -0600

    getting form set up

--- app/forms/entry_form.py
+++ app/forms/entry_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
@@ -24,15 +24,16 @@ from app.models import User
 
 
 class EntryForm(FlaskForm):
-    category_id = StringField('category_id')
+    category_id = StringField('category_id', validators=[DataRequired()])
     company_id = StringField('company_id')
     first_name = StringField('first_name', validators=[DataRequired()])
     last_name = StringField('last_name', validators=[DataRequired()])
     phone_number = StringField('phone_number', validators=[DataRequired()])
     cell_number = StringField('cell_number')
-    email = StringField('email', validators=[Email])
+    email = StringField('email', validators=[Email()])
     fax_number = StringField('fax_number')
     city_id = StringField('city_id')
     state_id = StringField('state_id')
     zip_id = StringField('zip_id')
-    note = StringField('note')
+    note = TextAreaField('note')
+    submit = SubmitField('submit')
--- react-app/src/components/NewEntryFormPage/index.js
+++ react-app/src/components/NewEntryFormPage/index.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
-// import { useEffect } from 'react';
+import { useState } from 'react';
 import { Redirect, useHistory } from 'react-router-dom';
 // import { getAllEntries } from '../../store/entries';
 // import "./SearchPage.css"
@@ -8,15 +8,57 @@ export default function NewEntryFormPage () {
     // const dispatch = useDispatch()
     const history = useHistory()
 	const sessionUser = useSelector(state => state.session.user);
-    // const searchResults = Object.values(useSelector(state => state.entries));
+    const [submitted, setSubmitted] = useState(false);
 
     if (!sessionUser) return <Redirect to='/login' />
+
+
+    const submitForm = async (e) => {
+        e.preventDefault();
+        setSubmitted(true);
+
+        const formData = new FormData();
+        // formData.append("text", text);
+
+        // const data = await dispatch(createPost(formData));
+        // if data is sent back set errors to the data
+        // if (data.errors) {
+        //     return setErrors(data.errors[0]);
+        // }
+
+        // if (submitted && errors) {
+        //     setErrors('');
+        // }
+        // setText('')
+        setSubmitted(false)
+    }
 
 
     return (
         <div className='new-entry-form-house'>
             <button onClick={() => history.goBack()}>Go back</button>
-            <p>This is where you make a new entry...</p>
+            <form onSubmit={submitForm} encType="multipart/form-data">
+                    <div className='new-entry-form'>
+                        <ul>
+                            {errors && (
+                                <p style={{ color: "red" }}>{errors}</p>
+                            )}
+                        </ul>
+                        {/* {
+                            company (id if associated already) - required
+                            category (id) - required
+                            subcategories (id's)
+                            contact first_name - required last_name - required
+                            contact email
+                            primary_phone - required
+                            secondary_phone
+                            fax_number
+                            city, state zip
+                            notes on entry
+                        } */}
+                        <button>Add entry</button>
+                    </div>
+            </form >
         </div>
     )
 }
