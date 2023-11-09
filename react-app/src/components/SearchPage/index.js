08d8d35ed0620b30e80d584138bcfdd13dc035e4
Jenna <jennagodfrey324@gmail.com>
2023-11-09 15:55:38 -0700

    little more styling around search page, now going to start of the filtering

--- react-app/src/components/SearchPage/index.js
+++ react-app/src/components/SearchPage/index.js
@@ -16,25 +16,32 @@ export default function SearchPage () {
 
     if (!sessionUser) return <Redirect to='/login' />
 
-    return searchResults.length ? (
+    return (
+
         <div className='search-res-house'>
             <button onClick={() => history.push('/entries/new')}>Make new entry</button>
-            {searchResults.map(item => {
-                return (
-                    <div key={item.id} className='entry-house'>
-                        <div className='entry-house-buttons'>
-                            <button onClick={() => history.push(`/entries/${item.id}/update`)}>Update</button>
-                            <button onClick={() => history.push(`/entries/${item.id}/delete`)}>Delete</button>
-                        </div>
-                        <h2>{item.company.company_name}</h2>
-                        <p><span>{item.location.city}</span>, <span>{item.location.state}</span> <span>{item.location.zip}</span></p>
-                        <h3>Contact: </h3>
-                        <p><span>{item.contact.first_name} {item.contact.last_name}</span> --- <span>{item.contact.phone_number}</span></p>
-                        <p>Notes: {item.notes}</p>
-                        <a onClick={() => history.push(`/entries/${item.id}`)}>Details</a>
-                    </div>
-                )
-            })}
+            {searchResults.length ? (
+                <>
+                    {searchResults.map(item => {
+                        return (
+                            <div key={item.id} className='entry-house'>
+                                <div className='entry-house-buttons'>
+                                    <button onClick={() => history.push(`/entries/${item.id}/update`)}>Update</button>
+                                    <button onClick={() => history.push(`/entries/${item.id}/delete`)}>Delete</button>
+                                </div>
+                                <div className='entry-house-details' onClick={() => history.push(`/entries/${item.id}`)}>
+                                    <h2>{item.company.company_name}</h2>
+                                    <p><span>{item.location.city}</span>, <span>{item.location.state}</span> <span>{item.location.zip}</span></p>
+                                    <h3>Contact: </h3>
+                                    <p><span>{item.contact.first_name} {item.contact.last_name}</span> --- <span>{item.contact.phone_number}</span></p>
+                                    <h3>Notes: </h3>
+                                    <p>{item.notes}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </>
+            ) : 'No Results'}
         </div>
-    ) : null
+    )
 }
