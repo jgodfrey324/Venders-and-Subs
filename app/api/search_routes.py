33fca28669d92f6fcd6fd0d4ff09c29daf60b724
Jenna <jennagodfrey324@gmail.com>
2023-11-15 13:32:11 -0700

    search filter is filtering

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from app.models import Entry, Company, Category
+from app.models import Entry, Company
 
 
 search_routes = Blueprint('search', __name__)
@@ -10,51 +10,98 @@ search_routes = Blueprint('search', __name__)
 @search_routes.route('/', methods=["POST"])
 # @login_required
 def search_results():
+    # getting request body
     request_body = request.get_json()['search']
 
-    print('request body 💖 --> ', request_body)
+    # query for all entries
+    entries = Entry.query.all()
 
-    entries = []
-
+    # if it's the initial load of search, give all entries
     if request_body == {}:
-        entries = Entry.query.all()
+        # flattening entries
+        results = {}
+        for entry in entries:
+            new_entry = entry.to_dict()
+            results[new_entry['id']] = new_entry
 
-    else:
-        company = request_body['companyName'].lower()
+        return results
 
-        contact_name = request_body['contactName'].split(' ')
-        first_name = contact_name[0]
-        last_name = contact_name[1] if len(contact_name) > 1 else ''
 
-        sub_cat = request_body['subCategory']
+    # breaking down request body...
+    company = request_body['companyName'].lower()
 
-        category = request_body['category']
+    contact_name = request_body['contactName'].split(' ')
+    first_name = contact_name[0]
+    last_name = contact_name[1] if len(contact_name) > 1 else ''
 
-        # grabbing all entries
-        # entries = Entry.query.all()
-        entries = []
+    sub_cat = request_body['subCategory']
+
+    category = request_body['category']
+
+
+    # making functions to handle each of the filters
+    def company_check(list):
         if company:
-            entries = Entry.query.join(Entry.company).filter(Company.company_name.ilike(f'{company}%'))
+            matched_entries = Entry.query.join(Entry.company).filter(Company.company_name.ilike(f'{company}%')).all()
 
-        # if len(contact_name) > 0:
-        #     entries = Entry.query.filter(Entry.first_name.ilike(f'{first_name}') | Entry.last_name.ilike(f'{first_name}') | Entry.first_name.ilike(f'{last_name}') | Entry.last_name.ilike(f'{first_name}'))
+            if len(matched_entries) > 0:
+                return matched_entries
+        return list
 
-        if category:
-            entries = Entry.query.join(Entry.category).filter(Category.category == category)
+    def first_name_check(list):
+        if first_name:
+            matched_entries = Entry.query.filter(Entry.first_name.ilike(f'{first_name}%')).all()
 
-        # print('entries 💖 -> ', entries)
+            if len(matched_entries) > 0:
+                return matched_entries
+        return list
 
-        # if len(sub_cat) > 0:
-        #     entries = filter(entries.sub_categories, sub_cat)
-        # entries = Entry.query.get(1)
+    def last_name_check(list):
+        if last_name:
+            matched_entries = Entry.query.filter(Entry.last_name.ilike(f'{last_name}%')).all()
 
-        # print('entry company 💖 --> ', entries.company.company_name)
+            if len(matched_entries) > 0:
+                return matched_entries
+        return list
 
-    # returning all entries flattened
-    results = {}
-    # to_dict()
-    for entry in entries:
-        new_entry = entry.to_dict()
-        results[new_entry['id']] = new_entry
 
-    return results
+    def category_check(entry):
+        entry_obj = entry.to_dict()
+
+        if not category:
+            return True
+        elif entry_obj['category'] == category:
+            return True
+        else:
+            return False
+
+    def sub_cat_check(entry):
+        entry_obj = entry.to_dict()
+
+        if len(sub_cat) == 0:
+            return True
+        elif len(sub_cat) > 0:
+            for cat in sub_cat:
+                if cat in entry_obj['sub_categories']:
+                    return True
+        else:
+            return False
+
+
+    # using filter function to join all these filters together
+    filtered_by_company = company_check(entries)
+    filtered_by_first_name = first_name_check(filtered_by_company)
+    filtered_by_last_name = last_name_check(filtered_by_first_name)
+    filtered_by_category = filter(category_check, list(filtered_by_last_name))
+    filtered_by_sub_cat = filter(sub_cat_check, list(filtered_by_category))
+
+    results = list(filtered_by_sub_cat)
+
+    # flatten results
+    return_obj = {}
+    if len(results) > 0:
+        for ele in results:
+                new_ele = ele.to_dict()
+                return_obj[new_ele['id']] = new_ele
+
+    return return_obj
