be6852874fc71b1cd714a5c544c2385ba405da10
Jenna <jennagodfrey324@gmail.com>
2023-11-16 15:47:06 -0700

    added users to seed files

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,20 +4,45 @@ from sqlalchemy.sql import text
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
-    morgan_ = User(
-        first_name='Morgan', last_name='Woodward', email='morgan@fixturestudio.com', password='password1')
+    morgan_woodward = User(
+        first_name='Morgan', last_name='Woodward', email='morgan@fixturestudio.com', password='fsmorgan')
     jami_huber = User(
-        first_name='Jami', last_name='Huber', email='jami@fixturestudio.com', password='password2')
+        first_name='Jami', last_name='Huber', email='jami@fixturestudio.com', password='fsjami')
     jeff_huber = User(
-        first_name='Jeff', last_name='Huber', email='jeff@fixturestudio.com', password='password3')
+        first_name='Jeff', last_name='Huber', email='jeff@fixturestudio.com', password='fsjeff')
     jenna_godfrey = User(
-        first_name='Jenna', last_name='Godfrey', email='jag@fixturestudio.com', password='password4'
+        first_name='Jenna', last_name='Godfrey', email='jag@fixturestudio.com', password='fsjennag'
+    )
+    chad = User(
+        first_name='Chad', last_name='Smith', email='chad@fixturestudio.com', password='jschad'
+    )
+    will = User(
+        first_name='Will', last_name='Smith', email='will@fixturestudio.com', password='fswill'
+    )
+    vern = User(
+        first_name='Vern', last_name='Smith', email='vern@fixturestudio.com', password='fsvern'
+    )
+    jackson = User(
+        first_name='Jackson', last_name='Huber', email='jackson@fixturestudio.com', password='fsjackson'
+    )
+    jenna_smith = User(
+        first_name='Jenna', last_name='Smith', email='jenna@fixturestudio.com', password='fsjenna'
+    )
+    erin_lutz = User(
+        first_name='Erin', last_name='Lutz', email='erin@fixturestudio.com', password='jserin'
     )
 
-    db.session.add(morgan_)
+    db.session.add(morgan_woodward)
     db.session.add(jami_huber)
     db.session.add(jeff_huber)
     db.session.add(jenna_godfrey)
+    db.session.add(chad)
+    db.session.add(will)
+    db.session.add(vern)
+    db.session.add(jackson)
+    db.session.add(jenna_smith)
+    db.session.add(erin_lutz)
+
     db.session.commit()
 
 
